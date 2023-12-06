7c5871429cd0e732f06e3fa82534fe5acf4a3955
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-06 08:28:24 +0300

    refactor day 6

--- src/day6.ts
+++ src/day6.ts
@@ -1,3 +1,4 @@
+import { assert } from 'node:console';
 import fs from 'node:fs';
 
 type Race = {
@@ -5,54 +6,40 @@ type Race = {
   distance: number;
 };
 
-type TInput = {
-  races: Race[];
-};
+type TInput = Race[];
 
-function parseInput(lines: string[]): TInput {
+function parseTimeAndDistance(lines: string[]): [string[], string[]] {
   const [_a, timeNumbersStr] = lines[0]!.split(':').map(x => x.trim());
   const [_b, distanceNumbersStr] = lines[1]!.split(":").map(x => x.trim());
-  const races = [];
-
   const timeNumbers = timeNumbersStr!.split(' ').map(x => x.trim()).filter(x => x !== '');
   const distanceNumbers = distanceNumbersStr!.split(' ').map(x => x.trim()).filter(x => x !== '');
+  return [timeNumbers, distanceNumbers];
+}
 
-  for (let i = 0; i < timeNumbers.length; i++) {
+function parseInput(lines: string[]): TInput {
+  const [times, distances] = parseTimeAndDistance(lines);
+  const races = [];
+
+  for (let i = 0; i < times.length; i++) {
     races.push({
-      time: +timeNumbers[i]!,
-      distance: +distanceNumbers[i]!,
+      time: +times[i]!,
+      distance: +distances[i]!,
     });
   }
 
-  return {
-    races,
-  };
+  return races;
 }
 
 function parseInput2(lines: string[]): TInput {
-  const [_a, timeNumbersStr] = lines[0]!.split(':').map(x => x.trim());
-  const [_b, distanceNumbersStr] = lines[1]!.split(":").map(x => x.trim());
-  const timeNumber = timeNumbersStr!.split(' ').map(x => x.trim()).filter(x => x !== '').join('');
-  const distanceNumber = distanceNumbersStr!.split(' ').map(x => x.trim()).filter(x => x !== '').join('');
-
-  return {
-    races: [
-      {
-        time: +timeNumber,
-        distance: +distanceNumber,
-      },
-    ],
-  };
+  const [time, distance] = parseTimeAndDistance(lines).map((x) => x.join(''));
+  return [{ time: +time!, distance: +distance! }];
 }
 
-function waysOfRace(race: Race): number {
+function winningCount(race: Race): number {
   let counter = 0;
 
   for (let i = 0; i <= race.time; i++) {
-    const speed = i;
-    const leftTime = race.time - i;
-    const totalDist = speed * leftTime;
-    if (totalDist > race.distance) {
+    if ((i * (race.time - i)) > race.distance) {
       counter++;
     }
   }
@@ -61,21 +48,13 @@ function waysOfRace(race: Race): number {
 }
 
 function solve1(lines: string[]): number {
-  const { races } = parseInput(lines);
-  let prod = 1;
-
-  for (const race of races) {
-    const ways = waysOfRace(race);
-    prod *= ways;
-  }
-
-  return prod;
+  const races = parseInput(lines);
+  return races.reduce((acc, race) => acc * winningCount(race), 1);
 }
 
 function solve2(lines: string[]): number {
-  const { races } = parseInput2(lines);
-  console.log(races)
-  return waysOfRace(races[0]!);
+  const races = parseInput2(lines);
+  return winningCount(races[0]!);
 }
 
 export function day6() {
@@ -83,4 +62,6 @@ export function day6() {
   const res = [solve1(lines), solve2(lines)];
   console.log(`Day 6 result 1: ${res[0]}`);
   console.log(`Day 6 result 2: ${res[1]}`);
+  assert(res[0] === 440000, 'part 1');
+  assert(res[1] === 26187338, 'part 2');
 }

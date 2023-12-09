a5128988cc9e364a6caddbd5b26b7214f81772a3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-09 10:53:51 +0300

    add day 9 part 2

--- src/day9.ts
+++ src/day9.ts
@@ -1,9 +1,5 @@
-function parseLine(line: string): number[] {
-  return line.trim().split(' ').map(x => +x.trim());
-}
-
 function parseInput(lines: string[]): number[][] {
-  return lines.map(parseLine);
+  return lines.map((line) => line.trim().split(' ').map(x => +x.trim()));
 }
 
 function diff(arr: number[]): number[] {
@@ -17,32 +13,41 @@ function diff(arr: number[]): number[] {
 }
 
 function nextValue(history: number[]): number {
-  const list: number[][] = [];
-  list.push([...history]);
+  const list: number[] = [history[history.length - 1]!];
   let tmp: number[] = diff(history);
 
   while (!tmp.every(x => x === 0)) {
-    list.push([...tmp]);
+    list.push(tmp[tmp.length - 1]!);
     tmp = diff(tmp);
   }
 
-  return list.map(x => x[x.length - 1]!).reduce((acc, x) => acc + x, 0);
+  return list.reduce((acc, x) => acc + x, 0);
+}
+
+function prevValue(history: number[]): number {
+  const list: number[] = [history[0]!];
+  let tmp = diff(history), prev = 0;
+
+  while (!tmp.every(x => x === 0)) {
+    list.push(tmp[0]!);
+    tmp = diff(tmp);
+  }
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    prev = list[i]! - prev;
+  }
+
+  return prev;
 }
 
 export const expected1 = 1_995_001_648;
 export function solve1(lines: string[]): number {
   const input = parseInput(lines);
-  let sum = 0;
-
-  for (const line of input) {
-    const next = nextValue(line);
-    sum += next;
-  }
-
-  return sum;
+  return input.reduce((acc, x) => acc + nextValue(x), 0);
 }
 
-export const expected2 = 0;
+export const expected2 = 988;
 export function solve2(lines: string[]): number {
-  return lines.length;
+  const input = parseInput(lines);
+  return input.reduce((acc, x) => acc + prevValue(x), 0);
 }

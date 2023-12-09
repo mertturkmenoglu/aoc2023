e1165175e5d986891662cc6596420137f223cd18
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-09 20:14:37 +0300

    refactor

--- src/day9.ts
+++ src/day9.ts
@@ -1,17 +1,8 @@
-let input: number[][] | null = null;
+const parseInput = (lines: string[]) => lines.map((line) => line.split(' ').map(Number));
 
-function parseInput(lines: string[]): number[][] {
-  if (!input) {
-    input = lines.map((line) => line.trim().split(' ').map(x => +x.trim()));
-  }
-  return input;
-}
+const diff = (arr: number[]) => new Array(arr.length).fill(0).map((_, i) => i).slice(1).map(i => arr[i]! - arr[i - 1]!);
 
-function diff(arr: number[]): number[] {
-  return new Array(arr.length).fill(0).map((_, i) => i).slice(1).map(i => arr[i]! - arr[i - 1]!);
-}
-
-function steps(history: number[], type: 'next' | 'prev'): number[] {
+function diffs(history: number[], type: 'next' | 'prev'): number[] {
   const i = type === 'next' ? -1 : 0;
   const list = [history.at(i)!];
   let tmp = diff(history);
@@ -24,24 +15,15 @@ function steps(history: number[], type: 'next' | 'prev'): number[] {
   return list;
 }
 
-function nextValue(history: number[]): number {
-  const list = steps(history, 'next');
-  return list.reduce((acc, x) => acc + x, 0);
-}
+const nextValue = (history: number[]) => diffs(history, 'next').reduce((acc, x) => acc + x, 0);
 
 function prevValue(history: number[]): number {
-  const list = steps(history, 'prev');
+  const list = diffs(history, 'prev');
   return new Array(list.length).fill(0).map((_, i, arr) => arr.length - i - 1).reduce((acc, i) => list[i]! - acc, 0);
 }
 
 export const expected1 = 1_995_001_648;
-export function solve1(lines: string[]): number {
-  const input = parseInput(lines);
-  return input.reduce((acc, x) => acc + nextValue(x), 0);
-}
+export const solve1 = (lines: string[]) => parseInput(lines).reduce((acc, x) => acc + nextValue(x), 0);
 
 export const expected2 = 988;
-export function solve2(lines: string[]): number {
-  const input = parseInput(lines);
-  return input.reduce((acc, x) => acc + prevValue(x), 0);
-}
+export const solve2 = (lines: string[]) => parseInput(lines).reduce((acc, x) => acc + prevValue(x), 0);

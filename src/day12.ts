70d584cede1817f5fd0b79bb0e140edad813df2d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-12 18:13:22 +0300

    add day 12 part 2

--- src/day12.ts
+++ src/day12.ts
@@ -1,59 +1,65 @@
-type Row = [chars: string[], groups: number[]];
 
-function parseInput(lines: string[]): Row[] {
+type Row = [springs: string, groups: number[]];
+
+function parseInput(lines: string[], sol: number): Row[] {
   return lines.map((line) => {
-    const [charsSec, groupsSec] = line.split(' ');
-    return [charsSec!.split(''), groupsSec!.split(',').map(Number)];
+    const [charsSec, groupsSec]= line.split(' ');
+    const chars: string[] = [];
+    const groups: string[]= [];
+
+    for (let i = 0; i < (sol === 1 ? 1 : 5); i++) {
+      chars.push(charsSec!);
+      groups.push(groupsSec!);
+    }
+
+    return [chars.join('?'), groups.join(',').split(',').map(Number)];
   });
 }
 
-function mutate(row: Row): string[] {
-  const [chars] = row;
-  const stack = [chars.join('')];
-  const res: Set<string> = new Set();
-
-  while (stack.length > 0) {
-    const s = stack.pop();
-    
-    if (!s!.includes('?')) {
-      res.add(s!);
-    } else {
-      const c = s!.split('');
-      const i = c.indexOf('?');
-      c[i] = '.';
-      stack.push(c.join(''));
-      c[i] = '#';
-      stack.push(c.join(''));
-    }
-  }
-
-  return [...res];
+function key(row: Row): string {
+  return `${row[0]}|${row[1].join()}`
 }
 
-function possibleArrangements(row: Row): number {
-  const [, groups] = row;
-  const mutations = mutate(row).filter(s => {
-    const sections = s.split('.').filter(x => x !== '');
-    return sections.length === groups.length;
-  })
-  let counter = 0;
+const cache = new Map<string, number>();
 
-  for (const m of mutations) {
-    const parts = m.split('.').filter(x => x !== '');
-    if (parts.every((x, i) => x.length === groups[i]!)) {
-      counter++;
-    }
+function count(row: Row): number {
+  const [config, groups] = row;
+
+  if (groups.length === 0) {
+    return config.includes('#') ? 0 : 1;
   }
 
-  return counter;
+  if (config.length === 0) {
+    return 0;
+  }
+
+  if (cache.has(key(row))) {
+    return cache.get(key(row))!;
+  }
+
+  let result = 0;
+  const ch = config[0]!;
+  const n = groups[0]!;
+
+  if (".?".includes(ch)) {
+    result += count([config.slice(1), groups]);
+  }
+
+  if ("#?".includes(ch) && n <= config.length && !config.slice(0, n).includes(".")
+    && (n === config.length || config[n] !== '#')) {
+    result += count([config.slice(n + 1), groups.slice(1)]);
+  }
+
+  cache.set(key(row), result);
+  return result;
 }
 
 export const expected1 = 8022;
 export function solve1(lines: string[]): number {
-  return parseInput(lines).map(possibleArrangements).reduce((acc, x) => acc + x, 0);
+  return parseInput(lines, 1).map(count).reduce((acc, x) => acc + x, 0);
 }
 
-export const expected2 = 0;
+export const expected2 = 4_968_620_679_637;
 export function solve2(lines: string[]): number {
-  return lines.length;
+  return parseInput(lines, 2).map(count).reduce((acc, x) => acc + x, 0);
 }

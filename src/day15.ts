28e7a9d6a8da0020bfd597b48e79d1d32540439e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-15 10:13:30 +0300

    add day 15 part 2

--- src/day15.ts
+++ src/day15.ts
@@ -1,3 +1,8 @@
+interface Lense {
+  label: string
+  focalLength: number
+}
+
 function hash(str: string): number {
   let currentValue = 0;
   const chars = str.split('');
@@ -16,12 +21,58 @@ function parseInput(line: string): string[] {
   return line.split(',');
 }
 
-export const expected1 = 0;
+function compute(operations: string[]): number {
+  const map = new Map<number, Lense[]>();
+
+  const equal = (op: string): void => {
+    const [label, focalLengthStr] = op.split('=');
+    const hashval = hash(label!);
+    const lenses = map.get(hashval);
+    const lense: Lense = { label: label!, focalLength: +focalLengthStr! };
+    if (lenses === undefined || lenses.length === 0) {
+      map.set(hashval, [lense]);
+      return;
+    }
+    const index = lenses.findIndex((v) => v.label === label);
+
+    if (index === -1) {
+      map.set(hashval, [...lenses, lense]);
+    } else {
+      lenses[index] = lense;
+      map.set(hashval, lenses);
+    }
+  };
+
+  const dash = (op: string): void => {
+    const label = op.substring(0, op.length - 1);
+    const hashval = hash(label);
+    const lenses = map.get(hashval) ?? [];
+    map.set(hashval, lenses.filter((v) => v.label !== label));
+  };
+
+  for (const operation of operations) {
+    if (operation.includes('=')) {
+      equal(operation);
+    } else {
+      dash(operation);
+    }
+  }
+
+  let sum = 0;
+
+  for (const [boxNumber, lenses] of map) {
+    sum += lenses.map((lense, i) => (1 + boxNumber) * (i + 1) * lense.focalLength).reduce((acc, x) => acc + x, 0);
+  }
+
+  return sum;
+}
+
+export const expected1 = 511_416;
 export function solve1(lines: string[]): number {
   return parseInput(lines[0]!).map(hash).reduce((acc, x) => acc + x, 0);
 }
 
-export const expected2 = 0;
+export const expected2 = 290_779;
 export function solve2(lines: string[]): number {
-  return lines.length;
+  return compute(parseInput(lines[0]!));
 }

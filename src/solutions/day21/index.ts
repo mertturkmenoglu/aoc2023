2a6c732c2d171ee47e4dcd7c1928722bf92d2340
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-12-21 14:37:52 +0300

    add day 21 part 2

--- src/solutions/day21/index.ts
+++ src/solutions/day21/index.ts
@@ -9,6 +9,7 @@ import {
 export class Solution extends AbstractSolution {
   private readonly grid: Grid<string>;
   private readonly targetSteps = 64;
+  private readonly actualTarget = 26_501_365;
 
   constructor(lines: string[]) {
     super(lines);
@@ -19,16 +20,9 @@ export class Solution extends AbstractSolution {
     return new Grid<string>(this.lines.map((line) => line.split('')));
   }
 
-  compute(): number {
-    const start = this.grid.getPosOfValue('S');
-
-    if (start === null) {
-      throw new Error('Cannot find starting position');
-    }
-
+  compute(start: Pos, target: number): number {
     const mapping = [...hvAdjMap]; // Horizontal and vertical
-
-    const queue: Array<[Pos, steps: number]> = [[start, this.targetSteps]];
+    const queue: Array<[Pos, steps: number]> = [[start, target]];
     const visited = new Set<string>();
     const result = new Set<string>();
     const s = JSON.stringify;
@@ -60,13 +54,72 @@ export class Solution extends AbstractSolution {
     return result.size;
   }
 
-  @Expect(0)
+  @Expect(3532)
   override solve1(): string | number {
-    return this.compute();
+    const start = this.grid.getPosOfValue('S');
+
+    if (start === null) {
+      throw new Error('Cannot find starting position');
+    }
+
+    return this.compute(start, this.targetSteps);
   }
 
-  @Expect(0)
+  @Expect(590_104_708_070_703)
   override solve2(): string | number {
-    return 0;
+    const start = this.grid.getPosOfValue('S');
+
+    if (start === null) {
+      throw new Error('Cannot find starting position');
+    }
+
+    const [r, c] = this.grid.dims();
+
+    if (r !== c) {
+      throw new Error('Grid has to be a square');
+    }
+
+    const middleCheck = start[0] === start[1] && start[0] === Math.floor(r / 2);
+
+    if (!middleCheck) {
+      throw new Error('Starting point has to be in the middle of the grid');
+    }
+
+    const stepsCheck = this.actualTarget % r === Math.floor(r / 2);
+
+    if (!stepsCheck) {
+      throw new Error('Invalid');
+    }
+
+    const f = Math.floor;
+
+    const gridWidth = f(this.actualTarget / r) - 1;
+    const odd = Math.pow(f(gridWidth / 2) * 2 + 1, 2);
+    const even = Math.pow(f((gridWidth + 1) / 2) * 2, 2);
+    const oddPoints = this.compute(start, r * 2 + 1);
+    const evenPoints = this.compute(start, r * 2);
+    const ct = this.compute([r - 1, start[1]], r - 1);
+    const cr = this.compute([start[0], 0], r - 1);
+    const cb = this.compute([0, start[1]], r - 1);
+    const cl = this.compute([start[0], r - 1], r - 1);
+    const str = this.compute([r - 1, 0], f(r / 2) - 1);
+    const stl = this.compute([r - 1, r - 1], f(r / 2) - 1);
+    const sbr = this.compute([0, 0], f(r / 2) - 1);
+    const sbl = this.compute([0, r - 1], f(r / 2) - 1);
+    const ltr = this.compute([r - 1, 0], f((r * 3) / 2) - 1);
+    const ltl = this.compute([r - 1, r - 1], f((r * 3) / 2) - 1);
+    const lbr = this.compute([0, 0], f((r * 3) / 2) - 1);
+    const lbl = this.compute([0, r - 1], f((r * 3) / 2) - 1);
+
+    return (
+      odd * oddPoints +
+      even * evenPoints +
+      ct +
+      cr +
+      cb +
+      cl +
+      (gridWidth + 1) * (str + stl + sbr + sbl) +
+      gridWidth * (ltr + ltl + lbr + lbl)
+    );
   }
 }
